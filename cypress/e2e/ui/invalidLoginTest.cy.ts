61874c319828883ab1b94f116af58a102f5322bf
Michał Woźniak <>
2026-02-27 10:52:52 +0100

    invalid login fixes

--- cypress/e2e/ui/invalidLoginTest.cy.ts
+++ cypress/e2e/ui/invalidLoginTest.cy.ts
@@ -1,12 +1,7 @@
 import { AuthPage } from '../../pageObjects/AuthPage';
-import { AccountCreationSuccessPage } from '../../pageObjects/AccountCreationSuccessPage';
 import { HomePage } from '../../pageObjects/HomePage';
-import { SignupPage } from '../../pageObjects/SignupPage';
-import { createRandomUser, User } from '../../testData/userFactory';
 
 describe('Regression | "Test Case 3: Login User with incorrect email and password"', () => {
-  let user: User;
-
   it('logs in with incorrect credentials', () => {
     const home = new HomePage();
     const auth = new AuthPage();
