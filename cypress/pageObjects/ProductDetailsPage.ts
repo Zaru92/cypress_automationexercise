609f48c388d9259c0859094bae1e052166431ac8
Michał Woźniak <>
2026-03-18 13:55:29 +0100

    ProductsPageTest fixes

--- cypress/pageObjects/ProductDetailsPage.ts
+++ cypress/pageObjects/ProductDetailsPage.ts
@@ -2,6 +2,12 @@ export class ProductDetailsPage {
   assertProductDetailsPageVisible() {
     cy.url().should('include', 'product_details');
     cy.get('.product-information').should('be.visible');
+    cy.get('h2').should('be.visible').and('not.be.empty');
+    cy.contains('Category:').should('be.visible');
+    cy.contains('Rs.').should('be.visible');
+    cy.contains('Availability:').should('be.visible');
+    cy.contains('Condition:').should('be.visible');
+    cy.contains('Brand:').should('be.visible');
     return this;
   }
 }
