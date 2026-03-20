ed8356bd9f09cc613e7f37d92676a3d1b6154877
Michał Woźniak <>
2026-03-20 15:30:38 +0100

    Subscription in Cart Page fixes

--- cypress/e2e/ui/TC11-subscriptionInCartPageTest.cy.ts
+++ cypress/e2e/ui/TC11-subscriptionInCartPageTest.cy.ts
@@ -2,8 +2,8 @@ import { HomePage } from '../../pageObjects/HomePage';
 import { CartPage } from '../../pageObjects/CartPage';
 import { createRandomUser } from '../../testData/userFactory';
 
-describe('Regression | Test Case 10: Verify Subscription in home page', () => {
-  it('submit scubsctiption and verify success message', () => {
+describe('Regression | Test Case 11: Verify Subscription in Cart page', () => {
+  it('submit scubsctiption in Cart page and verify success message', () => {
     const home = new HomePage();
     const cart = new CartPage();
     const user = createRandomUser();
