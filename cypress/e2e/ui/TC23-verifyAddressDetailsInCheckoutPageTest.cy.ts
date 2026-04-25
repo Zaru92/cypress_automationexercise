6024dc99024416838011ebc5e9b42fd8ef6bd41c
Michał Woźniak <>
2026-04-25 22:15:10 +0200

    Verify Address Details In Checkout Page Test

--- cypress/e2e/ui/TC23-verifyAddressDetailsInCheckoutPageTest.cy.ts
+++ cypress/e2e/ui/TC23-verifyAddressDetailsInCheckoutPageTest.cy.ts
@@ -1,6 +1,4 @@
 import { createRandomUser } from '../../testData/userFactory';
-import { createRandomContactMessage } from '../../testData/contactFactory';
-import { createRandomPaymentDetails } from '../../testData/paymentFactory';
 
 import { HomePage } from '../../pageObjects/HomePage';
 import { AuthPage } from '../../pageObjects/AuthPage';
@@ -8,14 +6,10 @@ import { SignupPage } from '../../pageObjects/SignupPage';
 import { AccountCreationSuccessPage } from '../../pageObjects/AccountCreationSuccessPage';
 import { CartPage } from '../../pageObjects/CartPage';
 import { CheckoutPage } from '../../pageObjects/CheckoutPage';
-import { PaymentPage } from '../../pageObjects/PaymentPage';
-import { OrderPlacementSuccessPage } from '../../pageObjects/OrderPlacementSuccessPage';
 
 describe('Regression | Test Case 23: Verify address details in checkout page', () => {
   it('register account before placing order', () => {
     const user = createRandomUser();
-    const data = createRandomContactMessage();
-    const paymentData = createRandomPaymentDetails();
 
     const home = new HomePage();
     const auth = new AuthPage();
@@ -23,8 +17,6 @@ describe('Regression | Test Case 23: Verify address details in checkout page', (
     const confirmation = new AccountCreationSuccessPage();
     const cart = new CartPage();
     const checkout = new CheckoutPage();
-    const payment = new PaymentPage();
-    const orderConfirmation = new OrderPlacementSuccessPage();
 
     home.visit().assertLoaded().goToSignupLoginPage();
 
