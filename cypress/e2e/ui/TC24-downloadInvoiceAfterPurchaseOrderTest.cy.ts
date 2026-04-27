515e8beb3d1d0f661a154b2131aeb7bab1e6a289
Michał Woźniak <>
2026-04-27 15:15:50 +0200

    Download Invoice After Purchase Order Test

---
+++ cypress/e2e/ui/TC24-downloadInvoiceAfterPurchaseOrderTest.cy.ts
@@ -0,0 +1,60 @@
+import { createRandomUser } from '../../testData/userFactory';
+import { createRandomContactMessage } from '../../testData/contactFactory';
+import { createRandomPaymentDetails } from '../../testData/paymentFactory';
+
+import { HomePage } from '../../pageObjects/HomePage';
+import { CartPage } from '../../pageObjects/CartPage';
+import { AuthPage } from '../../pageObjects/AuthPage';
+import { SignupPage } from '../../pageObjects/SignupPage';
+import { AccountCreationSuccessPage } from '../../pageObjects/AccountCreationSuccessPage';
+import { CheckoutPage } from '../../pageObjects/CheckoutPage';
+import { PaymentPage } from '../../pageObjects/PaymentPage';
+import { OrderPlacementSuccessPage } from '../../pageObjects/OrderPlacementSuccessPage';
+
+describe('Regression | Test Case 14: Place Order: Register while Checkout', () => {
+  it('register account during placing order', () => {
+    const user = createRandomUser();
+    const data = createRandomContactMessage();
+    const paymentData = createRandomPaymentDetails();
+
+    const home = new HomePage();
+    const cart = new CartPage();
+    const auth = new AuthPage();
+    const signup = new SignupPage();
+    const confirmation = new AccountCreationSuccessPage();
+    const checkout = new CheckoutPage();
+    const payment = new PaymentPage();
+    const orderConfirmation = new OrderPlacementSuccessPage();
+
+    home.visit().assertLoaded().addToCart(1).continueShopping().addToCart(2).viewCart();
+
+    cart.assertCartPageVisible().proceedToCheckout().goToAuthPage();
+
+    auth
+      .assertLoginOrSignupPageVisible()
+      .enterSignupName(user.name)
+      .enterSignupEmail(user.email)
+      .clickSignupButton();
+
+    signup
+      .assertSignupFormVisible()
+      .fillAccountInformation(user)
+      .selectNewsletterAndOffers()
+      .fillAddressDetails(user)
+      .confirmAccountCreation();
+
+    confirmation.assertAccountCreated().continueAfterCreation();
+
+    home.assertLoaded().assertLoggedInAs(user.name).goToCartPage();
+
+    cart.assertCartPageVisible().proceedToCheckout();
+
+    checkout.assertCheckoutPageVisible().assertAddressDetails(user).fillForm(data).placeOrder();
+
+    payment.assertPaymentPageVisible().fillForm(paymentData).submit();
+
+    orderConfirmation.assertOrderPlaced().downloadInvoice().continueAfterPlacement();
+
+    home.assertLoaded().deleteAccount().continueAfterDeleted().assertAccountDeleted(user.name);
+  });
+});
--- cypress/pageObjects/OrderPlacementSuccessPage.ts
+++ cypress/pageObjects/OrderPlacementSuccessPage.ts
@@ -2,13 +2,16 @@ export class OrderPlacementSuccessPage {
   assertOrderPlaced() {
     cy.url().should('include', 'payment_done');
     cy.contains('Order Placed!').should('be.visible');
-
     return this;
   }
 
   continueAfterPlacement() {
     cy.get('[data-qa="continue-button"]').click();
+    return this;
+  }
 
+  downloadInvoice() {
+    cy.get('.check_out').click();
     return this;
   }
 }
